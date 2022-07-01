76b7834bec325901f80d29876c06a655e443e35b
lspigariol <lspigariol@gmail.com>
2022-07-01 09:20:03 -0300

    codigobase

--- peliculas.pl
+++ peliculas.pl
@@ -4,14 +4,34 @@ personajeDeFiccion(harryPotter).
 animal(perro). 
 animal(gato). 
 
+quiniela(17,desgracia).
+quiniela(15,niniabonita).
+
+rol(verde,buscador).
+rol(rojo,defensor).
+
+
 pintura(verde,pared,kiosco).
+pintura(violeta,pelota,kiosco).
 
 gusta(harryPotter, verde).
 gusta(harryPotter, amarillo).
 gusta(frodo, verde).
 gusta(frodo, perro).
+gusta(frodo, violeta).
+gusta(frodo, 17).
 
-gusta(frodo,[verde,perro]).
+
+jugar(quiniela,Personaje,X):-
+    gusta(Personaje,Algo),
+    quiniela(Algo,X).
+jugar(quidich,Personaje,Rol):-
+    gusta(Personaje,Algo),
+    rol(Algo,Rol).
+jugar(Especie,Personaje,Especie):-
+    gusta(Personaje,Especie),
+    animal(Especie).
+
 
 nota(harryPotter,artesOscuras,7,fecha(marzo,31,2000)).
 nota(harryPotter,pociones,10,fecha(junio,27,2000)).
@@ -22,18 +42,14 @@ rindieronMismoDia(Mago,OtroMago):-
     nota(OtroMago,_,_,Fecha),
     Mago \= OtroMago.
 
-
-
 abogado("Juan"). 
 abogado("Pedro"). 
 
-saltos(frodo,[saltaMontania,saltoAnillo,saltoDuende]).
+saltos(frodo,[saltoMontania,saltoAnillo,saltoDuende]).
 
-
-
-saltos2(frodo,saltaMontania).
-saltos2(frodo,saltoAnillo).
-saltos2(frodo,saltoDuende).
+saltos2(frodo,saltoMontania).
+saltos2(duendeMalo,saltoAnillo).
+saltos2(gandalf,saltoDuende).
 
 dibujo( coordenada(3,4), circulo, 3 ).
 colectivo(esquina, "Juan", [1,2,3]).
@@ -51,8 +67,13 @@ cuantosSaltos2(Alguien,Cantidad):-
     findall( Salto, saltos2(Alguien,Salto), Saltos ),
     length(Saltos,Cantidad).
 
-ubicacionSalto(saltaMontania,coordenada(3,4)).
-ubicacionSalto(saltaAnillo,coordenada(13,40)).
+ubicacionSalto(saltoMontania,coordenada(3,4)).
+ubicacionSalto(saltoAnillo,coordenada(13,40)).
+ubicacionSalto(saltoAnillo,coordenada3D(50,40,10)).
+ubicacionSalto(saltoMontania,coordenada3D(13,40,100)).
+ubicacionSalto(saltoDuende,centro(tierrasBajas)).
+ubicacionSalto(saltoDuende,centro(tierrasAltas)).
+
 
 hizoUnBuenSalto(Alguien):-
     saltos2(Alguien,Salto),
@@ -62,6 +83,15 @@ hizoUnBuenSalto(Alguien):-
 posicionValida(coordenada(X,Y)):-
     numero(X),
     numero(Y).
+
+posicionValida(centro(Lugar)):-
+    esApto(Lugar).
+
+posicionValida(coordenada3D(A,B,C)):-
+    A is C + B.
+
+esApto(tierrasAltas).
+
 
 numero(3).
 numero(4).
